65cb18e6871d542c6b6eb181149d9c1d4e27aca1
Artem <aw.markevich@gmail.com>
2019-10-17 10:17:41 +0300

    'solution'

---
+++ precache-manifest.a9199d73b1992cd25e4771348478036c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732f48a1201f5c64f7b997ef8a84b8f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8b8cafee8b7c609f6e2",
+    "url": "./static/css/main.583cf3c0.chunk.css"
+  },
+  {
+    "revision": "c2963c5fdf95b41dde21",
+    "url": "./static/js/2.42311c15.chunk.js"
+  },
+  {
+    "revision": "f8b8cafee8b7c609f6e2",
+    "url": "./static/js/main.5e5dd65a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
